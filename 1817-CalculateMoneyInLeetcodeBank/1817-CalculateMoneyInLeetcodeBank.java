d702c190d2b8c182974a6ee493adbf1142b99197
Amritansh Singh <153822055+asg-01@users.noreply.github.com>
2025-10-29 03:06:43 +0530

    Create 1817-CalculateMoneyInLeetcodeBank

---
+++ 1817-CalculateMoneyInLeetcodeBank/1817-CalculateMoneyInLeetcodeBank.java
@@ -0,0 +1,18 @@
+// Last updated: 10/29/2025, 3:07:32 AM
+class Solution {
+    public int totalMoney(int n) {
+        int ans = 0;
+        int monday = 1;
+        
+        while (n > 0) {
+            for (int day = 0; day < Math.min(n, 7); day++) {
+                ans += monday + day;
+            }
+            
+            n -= 7;
+            monday++;
+        }
+        
+        return ans;
+    }
+}

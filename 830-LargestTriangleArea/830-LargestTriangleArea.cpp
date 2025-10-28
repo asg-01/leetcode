7c4e9f8a51372847f98ae68157246d5ab0a19f45
Amritansh Singh <153822055+asg-01@users.noreply.github.com>
2025-10-29 03:07:29 +0530

    Create 830-LargestTriangleArea

---
+++ 830-LargestTriangleArea/830-LargestTriangleArea.cpp
@@ -0,0 +1,17 @@
+// Last updated: 10/29/2025, 3:08:19 AM
+class Solution {
+public:
+    double largestTriangleArea(vector<vector<int>>& points) {
+        double res = 0;
+        for (int i = 0; i < points.size(); i++) {
+            for (int j = i + 1; j < points.size(); j++) {
+                for (int k = j + 1; k < points.size(); k++) {
+                    res = max(res, 0.5 * abs(points[i][0] * (points[j][1] - points[k][1]) +
+                                             points[j][0] * (points[k][1] - points[i][1]) +
+                                             points[k][0] * (points[i][1] - points[j][1])));
+                }
+            }
+        }
+        return res;
+    }
+};

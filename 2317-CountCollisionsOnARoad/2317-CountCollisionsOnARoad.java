89d917c704211169c18446a6c6e4c34b639f26fe
Amritansh Singh <153822055+asg-01@users.noreply.github.com>
2025-12-17 00:05:53 +0530

    Create 2317-CountCollisionsOnARoad

---
+++ 2317-CountCollisionsOnARoad/2317-CountCollisionsOnARoad.java
@@ -0,0 +1,18 @@
+// Last updated: 12/17/2025, 12:08:01 AM
+class Solution {
+    public int countCollisions(String dir) {
+        
+        int res = 0, n = dir.length(), i = 0, carsFromRight = 0;
+        
+        while (i < n && dir.charAt(i) == 'L') i++;
+        
+        for ( ; i<n; i++) {
+            if (dir.charAt(i) == 'R')  carsFromRight++;
+            else {
+                res += (dir.charAt(i) == 'S') ? carsFromRight : carsFromRight+1;
+                carsFromRight = 0;
+            }
+        }
+        return res;
+    }
+}

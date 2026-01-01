0e25c5a5d89ca93be3e0d77d6ff8e1e3649fb582
Amritansh Singh <153822055+asg-01@users.noreply.github.com>
2026-01-01 05:32:46 +0530

    File updated at 1/1/2026, 5:33:01 AM

--- 66-PlusOne/66-PlusOne.java
+++ 66-PlusOne/66-PlusOne.java
@@ -1,28 +1,28 @@
-// Last updated: 10/29/2025, 3:08:37 AM
-class Solution {
-    public int[] plusOne(int[] digits) {
-        // Get the length of the array
-        int n = digits.length;
-
-        // Iterate from the last digit to the first
-        for (int i = n - 1; i >= 0; i--) {
-            // Increment the current digit
-            digits[i]++;
-            
-            // If the digit is less than 10, there's no carry. We're done!
-            if (digits[i] < 10) {
-                return digits;
-            }
-            
-            // If the digit became 10, set it to 0 and the loop will carry over to the next digit
-            digits[i] = 0;
-        }
-        
-        // If we get here, it means the original number was all 9s (e.g., 9, 99, 999)
-        // We need a new array with one more digit
-        int[] newNumber = new int[n + 1];
-        newNumber[0] = 1; // Set the first digit to 1
-        
-        return newNumber;
-    }
-}
+// Last updated: 1/1/2026, 5:33:01 AM
+1class Solution {
+2    public int[] plusOne(int[] digits) {
+3        // Get the length of the array
+4        int n = digits.length;
+5
+6        // Iterate from the last digit to the first
+7        for (int i = n - 1; i >= 0; i--) {
+8            // Increment the current digit
+9            digits[i]++;
+10            
+11            // If the digit is less than 10, there's no carry. We're done!
+12            if (digits[i] < 10) {
+13                return digits;
+14            }
+15            
+16            // If the digit became 10, set it to 0 and the loop will carry over to the next digit
+17            digits[i] = 0;
+18        }
+19        
+20        // If we get here, it means the original number was all 9s (e.g., 9, 99, 999)
+21        // We need a new array with one more digit
+22        int[] newNumber = new int[n + 1];
+23        newNumber[0] = 1; // Set the first digit to 1
+24        
+25        return newNumber;
+26    }
+27}

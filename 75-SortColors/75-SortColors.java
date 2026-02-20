0013b9762c9a5b4d7d38979cf1f679c088c2c1ee
Amritansh Singh <153822055+asg-01@users.noreply.github.com>
2026-02-21 01:58:22 +0530

    File updated at 2/21/2026, 1:58:39 AM

--- 75-SortColors/75-SortColors.java
+++ 75-SortColors/75-SortColors.java
@@ -1,19 +1,22 @@
-// Last updated: 10/29/2025, 3:08:36 AM
-class Solution {
-    public void sortColors(int[] nums) {
-
-        for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-             
-                    if (nums[i] > nums[j]) {
-                        int temp = nums[i];
-                        nums[i] = nums[j];
-                        nums[j] = temp;
-                    
-                }
-
-            }
-        }
-
-    }
-}
+// Last updated: 2/21/2026, 1:58:39 AM
+1class Solution {
+2    public void sortColors(int[] nums) {
+3        HashMap<Integer, Integer> count = new HashMap<>();
+4        count.put(0, 0);
+5        count.put(1, 0);
+6        count.put(2, 0);
+7
+8        for (int num : nums) {
+9            count.put(num, count.get(num) + 1);
+10        }
+11
+12        int idx = 0;
+13        for (int color = 0; color < 3; color++) {
+14            int freq = count.get(color);
+15            for (int j = 0; j < freq; j++) {
+16                nums[idx] = color;
+17                idx++;
+18            }
+19        }        
+20    }
+21}
